263bab8864d927b78c212462f8a462b88363cebf
asvartgroupspb@gmail.com <asvartgroupspb@gmail.com>
2022-01-06 19:58:10 +0300

    Добавьте файлы проекта.

---
+++ Task_17/AccountOwner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_17
+{
+    class AccountOwner<T>
+    {
+        public T AccountNumber { get; set; }
+        public double AccountBalance { get; set; }
+        public string Name { get; set; }
+
+        public AccountOwner (T accountNumber, double accountBalance, string name)
+        {
+            AccountNumber = accountNumber;
+            AccountBalance = accountBalance;
+            Name = name;
+        }
+
+        public string GetInfo()
+        {
+            return $"{ AccountNumber } { AccountBalance } { Name }";
+
+        }
+    }
+}
---
+++ Task_17/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числовой номер\t");
+            int accountNumberInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите баланс счета\t");
+            double accountBalance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО\t");
+            string name = Console.ReadLine();
+            
+            AccountOwner<int> accountOwner1 = new AccountOwner<int> (accountNumberInt, accountBalance, name );
+            Console.WriteLine(accountOwner1.GetInfo()+ "\n");
+            Console.WriteLine();
+
+            Console.WriteLine("Введите строковый номер\t");
+            string accountNumberString = Console.ReadLine();
+            Console.WriteLine("Введите баланс счета\t");
+            accountBalance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО\t");
+            name = Console.ReadLine();
+
+            AccountOwner<string> accountOwner2 = new AccountOwner<string>(accountNumberString, accountBalance, name);
+            Console.WriteLine(accountOwner2.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
